719d1810e951ec4829343c2f522e12bab05f0659
Lucas Honorio <lucashonorioo@hotmail.com>
2025-08-27 17:59:03 -0300

    Implementing security configuration classes and entities

---
+++ src/main/java/com/devsuperior/bds04/entities/Role.java
@@ -0,0 +1,61 @@
+package com.devsuperior.bds04.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    @ManyToMany(mappedBy = "roles")
+    Set<User> users = new HashSet<>();
+
+    public Role(){
+
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, authority);
+    }
+}
